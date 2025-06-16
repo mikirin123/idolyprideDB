7bdabb37277c290198d721ad2240c9987cd13665
mikirin267 <12minecraft02@gmail.com>
2025-06-16 14:42:49 +0900

    追加

---
+++ event/はちはじ25/circlelist.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bannerTitle = document.querySelector('.banner_title');
+    bannerTitle.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    // スクロールトップボタンの追加
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = 'block';
+        } else {
+            scrollToTopBtn.style.display = 'none';
+        }
+    });
+
+    scrollToTopBtn.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
